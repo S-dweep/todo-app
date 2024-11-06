db5df1033ae62ed5d881c753d5e76da918be3dda
S-dweep <mondalsayandeep2004@gmail.com>
2024-11-06 19:31:31 +0530

    checking to complete feature added

--- web.py
+++ web.py
@@ -13,8 +13,13 @@ def add_todo():
 st.title("To-Do App")
 st.write("Become organised. Become productive.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    check = st.checkbox(todo, key=todo)
+    if check:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
